2d8dbd23a13b749a9a627f12490d3dc9b85b73bf
ujjawala397 <ujjawala397@gmail.com>
2022-07-09 15:57:12 -0400

    posts comment react-hook-form trial

--- client/src/components/dummyFiles/CommentForm.js
+++ client/src/components/dummyFiles/CommentForm.js
@@ -2,36 +2,31 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { addComment } from '../../actions/post';
-import {useForm} from 'react-hook-form';
 
 const CommentForm = ({ postId, addComment }) => {
   const [text, setText] = useState('');
-  const { register, handleSubmit } = useForm({ 
-    defaultValues: text
-  });
-  const onSubmit = (data) => {
-    addComment(data.postId, { text });
-    setText('');
-  };
 
   return (
     <div className='post-form'>
       <div className='bg-primary p'>
         <h3>Leave a Comment</h3>
       </div>
-      <form className="form my-1" onSubmit={handleSubmit(onSubmit)}>
-      
-     
-      
-        <Input
-          // name='text'
-          // cols='30'
-          // rows='5'
+      <form
+        className='form my-1'
+        onSubmit={e => {
+          e.preventDefault();
+          addComment(postId, { text });
+          setText('');
+        }}
+      >
+        <textarea
+          name='text'
+          cols='30'
+          rows='5'
           placeholder='Comment the post'
-          // value={text}
-          // onChange={e => setText(e.target.value)}
-          // required
-          {...register('text')}
+          value={text}
+          onChange={e => setText(e.target.value)}
+          required
         />
         <input type='submit' className='btn btn-dark my-1' value='Submit' />
       </form>
@@ -47,9 +42,3 @@ export default connect(
   null,
   { addComment }
 )(CommentForm);
-
-const Input = ({ type = 'text', placeholder, ...rest }) => {
-  return (
-    <textarea type={type} placeholder={placeholder} {...rest} />
-  )
-}
